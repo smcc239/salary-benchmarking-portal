3ae07745083bbde2d30e8a20ac6690b5a2d10235
smcc239 <smcc239@gmail.com>
2025-05-28 22:19:52 +0100

    Cap A

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import SalaryBenchmarkingPortal from './SalaryBenchmarkingPortal';
+
+function App() {
+  return (
+    <div className="App">
+      <SalaryBenchmarkingPortal />
+    </div>
+  );
+}
+
+export default App;
