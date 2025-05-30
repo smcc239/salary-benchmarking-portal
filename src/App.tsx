eb2f181f49b81d87ea60f1fa35d2eb26ebf7525f
smcc239 <smcc239@gmail.com>
2025-05-30 23:36:15 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -24,29 +24,27 @@ const App: React.FC = () => {
     setUser(loggedInUser);
   };
 
-  if (!user) {
-    return (
-      <div className="min-h-screen bg-gray-900 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
-        <div className="sm:mx-auto sm:w-full sm:max-w-md">
-          <h2 className="mt-6 text-center text-3xl font-extrabold text-white">
-            Salary Benchmarking Portal
-          </h2>
-        </div>
-
-        <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
-          <div className="bg-gray-800 py-8 px-4 shadow sm:rounded-lg sm:px-10">
-            <LoginForm onLogin={handleLogin} />
-          </div>
-        </div>
-      </div>
-    );
-  }
-
   return (
     <BrowserRouter>
-      <Layout user={user}>
-        <AppRoutes />
-      </Layout>
+      {!user ? (
+        <div className="min-h-screen bg-gray-900 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+          <div className="sm:mx-auto sm:w-full sm:max-w-md">
+            <h2 className="mt-6 text-center text-3xl font-extrabold text-white">
+              Salary Benchmarking Portal
+            </h2>
+          </div>
+
+          <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+            <div className="bg-gray-800 py-8 px-4 shadow sm:rounded-lg sm:px-10">
+              <LoginForm onLogin={handleLogin} />
+            </div>
+          </div>
+        </div>
+      ) : (
+        <Layout user={user}>
+          <AppRoutes />
+        </Layout>
+      )}
     </BrowserRouter>
   );
 };
