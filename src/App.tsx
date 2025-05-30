f9f79c56135bbe5b3e57fc18e1734bcaec778aa8
smcc239 <smcc239@gmail.com>
2025-05-30 23:13:42 +0100

    a

--- src/App.tsx
+++ src/App.tsx
@@ -41,23 +41,19 @@ const App: React.FC = () => {
     );
   }
 
-  const routes = (
-    <Routes>
-      <Route path="/" element={<Navigate to="/roles" replace />} />
-      <Route path="/roles" element={<div>Job Roles</div>} />
-      <Route path="/roles/themes" element={<div>Job Themes</div>} />
-      <Route path="/roles/single" element={<div>Single Job</div>} />
-      <Route path="/reports" element={<div>Reports</div>} />
-      <Route path="/reports/single" element={<div>Single Reports</div>} />
-      <Route path="/profile" element={<div>Profile</div>} />
-      <Route path="/profile/settings" element={<div>Profile Settings</div>} />
-    </Routes>
-  );
-
   return (
     <BrowserRouter>
       <Layout user={user}>
-        {routes}
+        <Routes>
+          <Route path="/" element={<Navigate to="/roles" replace />} />
+          <Route path="/roles" element={<div>Job Roles</div>} />
+          <Route path="/roles/themes" element={<div>Job Themes</div>} />
+          <Route path="/roles/single" element={<div>Single Job</div>} />
+          <Route path="/reports" element={<div>Reports</div>} />
+          <Route path="/reports/single" element={<div>Single Reports</div>} />
+          <Route path="/profile" element={<div>Profile</div>} />
+          <Route path="/profile/settings" element={<div>Profile Settings</div>} />
+        </Routes>
       </Layout>
     </BrowserRouter>
   );
