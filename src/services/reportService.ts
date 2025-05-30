7ce40f6fee0175acfda87cab8c3806cac6a73cdb
smcc239 <smcc239@gmail.com>
2025-05-30 23:00:21 +0100

    Update reportService.ts

--- src/services/reportService.ts
+++ src/services/reportService.ts
@@ -1,6 +1,7 @@
 import { BenchmarkReport } from '../types/survey';
 import { generateMockBenchmarkReport } from '../utils/reportGenerator';
+import { JobTheme } from '../types/job';
 
-export const generateReport = (roleId: string, roleTitle: string, theme: string): BenchmarkReport => {
+export const generateReport = (roleId: string, roleTitle: string, theme: JobTheme): BenchmarkReport => {
   return generateMockBenchmarkReport(roleId, roleTitle, theme);
 }; 
